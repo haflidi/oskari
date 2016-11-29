2a0f8239f19bcc23585a83df0ed6031cc88dcc4e
Hafliði Sigtryggur Magnússon <haflidi@tolvu.net>
2016-11-29 17:06:04 +0000

    initial push of is.js, just a copy of en.js

---
+++ bundles/mapping/drawtools/resources/locale/is.js
@@ -0,0 +1,9 @@
+Oskari.registerLocalization(
+{
+    "lang": "is",
+    "key": "DrawTools",
+    "value": {
+        "intersectionNotAllowed": "Polygon self-intersection is not allowed. Draw a valid polygon to see the measurement result."
+    }
+}
+);
